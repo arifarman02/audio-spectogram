f9ac669a338f7c25515a2331dcf43c1feb166cf5
arifarman02 <arifarman1820@gmail.com>
2025-05-08 13:00:16 +0100

    Working on audio playback

---
+++ app/source.py
@@ -0,0 +1,63 @@
+import librosa
+import math
+import numpy as np
+import pyaudio
+import time
+
+from utils import logger
+
+
+
+class Source:
+    
+    def __init__(self, *args, **kwargs):
+        self.audio = pyaudio.PyAudio
+        self.complete = False
+        self.data = []
+        self.index = 0
+        self.total = 0
+        self.init(*args, **kwargs)
+    
+    def init(*args, **Kwargs):
+        raise NotImplementedError('source.init')
+    
+    def callback(self, data, frame_count, time_info, status):
+        raise NotImplementedError('source.callback')
+    
+
+
+SAMPLE_RATE = 22050
+BUFFER_SIZE = 1024
+
+
+
+class File(Source):
+    
+    def init(self, filename):
+        self.data, _ = librosa.load(filename, sr=SAMPLE_RATE)
+        self.stream = self.audio.open(
+            format=pyaudio.paFloat32,
+            channels=1,
+            rate=SAMPLE_RATE,
+            output=True,
+            frames_per_buffer=BUFFER_SIZE,
+            stream_callback=self.callback)
+    
+    def callback(self, data, frame_count, time_info, status):
+        a = self.total
+        b = self.total + BUFFER_SIZE
+        data = self.data(a:b)
+        self.total = b
+        if self.total >= len(self.data):
+            self.complete = True
+        return (data, pyaudio.paContinue)
+
+
+
+class Microphone(Source):
+    pass
+
+
+
+if __name__ == '__main__':
+    filename = ''
