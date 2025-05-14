16989cb80e468db0037484a9b1efe721469f6a5a
arifarman02 <arifarman1820@gmail.com>
2025-05-14 14:19:32 +0100

    Added frequency and time axix panels

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import config
 
+from rect import Rect
 from source import File, Microphone
 from spec import Spec
 from wave import Wave
@@ -18,10 +19,22 @@ class App(Window):
        self.wave = Wave(self.ctx, 0, 0, config.WINDOW_WIDTH, 200)
        self.spec = Spec(self.ctx, 0, self.wave.h, config.WINDOW_WIDTH, 460)
 
+
+       bg_color = (0.06, 0.06, 0.07, 1)
+
+       # Time axis background colour
+       self.r1 = Rect(self.ctx, 0, 660, config.WINDOW_WIDTH, 70, bg_color)
+
+       # Frequency axis background colour
+       self.r2 = Rect(self.ctx, 0, 0, 80, config.WINDOW_HEIGHT, bg_color)
+
+
     def size(self, w, h):
         logger.info(f'size {w} {h}')
         self.wave.size(w, h)
         self.spec.size(w, h)
+        self.r1.size(w, h)
+        self.r2.size(w, h)
 
     def draw(self, dt):
         available = self.source.available()
@@ -37,6 +50,9 @@ class App(Window):
 
         self.wave.draw()
         self.spec.draw()
+
+        self.r1.draw()
+        self.r2.draw()
     
     def exit(self):
         logger.info('exit')
